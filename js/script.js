64c28a917abcee301ea85d8bc187ad6fc4b2bb89
baxter29 <sashapawlow@mail.ru>
2019-03-29 16:34:26 +0300

    Update
    Was added JQuery code.Add changes to HTML/CSS

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
 
-let flag = 0;
+$(".nav-menu__item[data-nav-menu='0']").on("click",function()
+{
+     $('html, body').animate({
+        scrollTop: $(".footer").offset().top
+    }, 2000);
+    
+})
 
-       $(".mobile-menu-burger").on("click",function()
-       {
-       	switch(flag)
-       	{
-       		case 0:
-       		$(".nav-menu-mobile").animate();
-       		flag = 1;
-       		
-       		break;
-       		case 1:
-       		$(".nav-menu-mobile").slideUp("slow");
-       		flag = 0;
-       		break;
-       	}
-       		
-       	}
-       )
-    })
+})
